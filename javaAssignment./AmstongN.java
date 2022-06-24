c4c5388dd66413ac7835d5a82d901e1eefcf07db
Yogesh Tamshete <105358887+YogeshTam@users.noreply.github.com>
2022-06-24 09:49:31 +0530

    Add files via upload

---
+++ javaAssignment./AmstongN.java
@@ -0,0 +1,28 @@
+public class AmstongN {
+    public static void main (String [] args)
+    {
+        //Program to find armstrong number from 100 to 999
+        //153, 370, 371, 407
+
+        for (int k = 100 ; k <= 1000 ; k++)
+        {
+            int n = k;
+            int d = 0;
+            int s = 0;
+
+            while (n > 0)
+            {
+                d = n % 10;
+                s = s + (d * d * d);
+                n = n / 10;
+            }
+            if (k == s)
+            {
+                System.out.println (k +"amstrong number");
+            }
+        }
+    } // main method
+} // Armstrong class
+
+
+
